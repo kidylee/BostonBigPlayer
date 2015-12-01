3daed5bec087e102bb354f603fe61f1714e31446
unknown <e524807@SSC7L6714152.corp.statestr.com>
2015-12-01 16:54:24 -0500

    remove core

--- BBP-core/src/main/java/com/kidylee/RedSox/OKCoin/Repository/FutureTickerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kidylee.RedSox.OKCoin.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kidylee.RedSox.OKCoin.domain.FutureTicker;
-
-public interface FutureTickerRepository extends JpaRepository<FutureTicker,Long> {
-
-}
--- BBP-core/src/main/java/com/kidylee/RedSox/service/WebSocketService.java
+++
@@ -1,8 +0,0 @@
-package com.kidylee.RedSox.service;
-
-public class WebSocketService {
-	
-	
-	
-	
-}
